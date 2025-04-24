a04002ac4d86c9dcfecffe73f8871620c2d2582e
Josh Gearheart <other.josh.gearheart@gmail.com>
2025-04-24 14:52:54 -0700

    update scroll logic

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(window).scroll(function() {
+function switchHeader() {
   if ($(window).scrollTop() >= 208) {
     $("#smallLogo").show();
   } else {
     $("#smallLogo").hide();
   }
-})
+}
+
+$(window).scroll(switchHeader);
+
+switchHeader();
